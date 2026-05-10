f201d73c86f2106ebcfdec2a2ed015edcdc102ae
Adham Ayad <aboayad557@gmail.com>
2026-05-10 18:24:07 +0300

    Add VPC module and related resources for infrastructure provisioning

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -3,3 +3,33 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1" 
 }
+
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+  default     = "KubeVision-Serverless-Platform"
+}
+
+variable "environment" {
+  description = "Environment (dev, staging, prod)"
+  type        = string
+  default     = "prod"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "Public subnets CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "Private subnets CIDRs"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
